592657b361c7db2a498d31eaddc1a9acc900d28b
kyliec31 <kyliec31@nycstudents.net>
2026-04-28 09:24:12 -0400

    4/28

---
+++ pet_project.py
@@ -0,0 +1,8 @@
+class Pet():
+    def _init_(self, name, happy, play, happiness):
+        self.name = name
+        self.happy = happy
+        self.play = play
+        self.happiness = happiness
+
+    def interact(self, )

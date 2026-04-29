9995a312bfcccc4577bec1ee0505603d46ddc438
kyliec31 <kyliec31@nycstudents.net>
2026-04-29 09:25:38 -0400

    4/29

--- pet_project.py
+++ pet_project.py
@@ -1,8 +1,22 @@
 class Pet():
-    def _init_(self, name, happy, play, happiness):
+    def _init_(self, name, happy, fetch, happiness):
         self.name = name
-        self.happy = happy
-        self.play = play
+        self.__happy = happy
+        self.fetch = fetch
         self.happiness = happiness
+    
 
-    def interact(self, )
+class Interact():
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+
+    def interact(self, fetch):
+        self.happiness.append(fetch)
+        print(self.happiness)
+
+Dog = Pet("Dog", 100, "fetch", 50)
+Dog.interact(50)
+
+print(Dog.__dict__)

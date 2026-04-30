e88369edb7e3afd95e935e071c2b94a340041985
kyliec31 <kyliec31@nycstudents.net>
2026-04-30 09:25:19 -0400

    4/30

--- pet_project.py
+++ pet_project.py
@@ -1,22 +1,36 @@
 class Pet():
-    def _init_(self, name, happy, fetch, happiness):
+    def _init_(self, name, age, happy):
         self.name = name
+        self.age = age
         self.__happy = happy
-        self.fetch = fetch
-        self.happiness = happiness
-    
 
-class Interact():
+    def interact(self):
+        return f"{self.name} is playing fetch"
+    
+"""     def happy_level(self, joy):
+        self.__happy += joy
+
+    def show_happy_level(self):
+        print(f"{self.name}'s happiness is now {self.__happy}")
 
     def buy(self, item):
         self.inventory.append(item)
-        print(self.inventory)
+        print(self.inventory) """
 
-    def interact(self, fetch):
-        self.happiness.append(fetch)
-        print(self.happiness)
 
-Dog = Pet("Dog", 100, "fetch", 50)
-Dog.interact(50)
+my_pet = Pet("Dog", "2", "50")
+print(my_pet.interact())
 
-print(Dog.__dict__)
+""" class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance  # double underscore means "private"
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
+Jillian =BankAccount("Jillian",2)
+Jillian.deposit(50)
+print(Jillian.__dict__) """
